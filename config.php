24793292c9689da7f2ed7e26a2945878c9df0d30
Koen <koen.declerck@student.howest.be>
2017-10-12 10:42:53 +0200

    Added basic pdo classes

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+class Config{
+
+  public static $siteURL = '';
+
+  public static $dbServer = '';
+  public static $dbPort = '';
+  public static $dbName = '';
+  public static $dbUser = '';
+  public static $dbPass = '';
+}
---
+++ graderdb.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('config.php');
+
+class graderdb {
+
+  public static function getConnection() {
+
+        // Construct the PDO adress line
+        $host = Config::$dbServer;
+        $port = Config::$dbPort;
+        $database = Config::$dbName;
+        $dsn = "mysql:host=$host;port=$port;dbname=$database";
+
+        // Try to connect to the database
+        $conn = new PDO($dsn, Config::$dbUser, Config::$dbPass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+
+}
+
+class UserDAO {
+  
+}
