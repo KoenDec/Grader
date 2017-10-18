5c0b8a756e903a3d13ec72b19de5847c392ce86d
Riwan Carpentier <riwan.carpentier@hotmail.com>
2017-10-18 15:37:40 +0200

    showStudents page now with db data

--- graderdb.php
+++ graderdb.php
@@ -64,7 +64,6 @@ class UserDAO {
     } else {
       die('No user with username = ' . $username);
     }
-
     return $user;
   }
 
@@ -89,7 +88,6 @@ class UserDAO {
     }
 
     return $user;
-
   }
 
   public static function createUser($firstname, $lastname, $email, $password) {
@@ -109,4 +107,82 @@ class UserDAO {
     }
 
   }
+
+    public static function getAllStudents(){
+        try {
+            $conn = graderdb::getConnection();
+
+            $sql = 'SELECT * FROM users WHERE id IN (SELECT studentId FROM studenten)';
+            $stmt = $conn->prepare($sql);
+
+            $stmt->execute();
+
+            $studentsTable = $stmt->fetchAll(PDO::FETCH_CLASS);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+
+        if(isset($studentsTable[0])) {
+            $students = $studentsTable;
+        } else {
+            die('No users found!');
+        }
+
+        return $students;
+    }
+
+    public static function getAllStudentsInEducation($educationName){
+        try {
+            $conn = graderdb::getConnection();
+
+            $sql = 'SELECT DISTINCT u.* FROM users u
+	                  JOIN studenten s on u.id = s.studentId
+	                  JOIN studenten_modules sm ON s.studentId = sm.studentId
+	                  JOIN opleidingen_modules om ON om.moduleId = sm.moduleId
+	                  JOIN opleidingen o ON o.id = om.opleidingId
+                      WHERE o.name = :educationName';
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':educationName',$educationName);
+
+            $stmt->execute();
+
+            $studentsTable = $stmt->fetchAll(PDO::FETCH_CLASS);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+
+        if(isset($studentsTable[0])) {
+            $students = $studentsTable;
+        } else {
+            die('No students found!');
+        }
+
+        return $students;
+    }
+
+
+
+    public static function getAllEducations(){
+        try {
+            $conn = graderdb::getConnection();
+
+            $sql = 'SELECT * FROM opleidingen';
+            $stmt = $conn->prepare($sql);
+
+            $stmt->execute();
+
+            $educationsTable = $stmt->fetchAll(PDO::FETCH_CLASS);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+
+        if(isset($educationsTable[0])) {
+            $educations = $educationsTable;
+        } else {
+            die('No educations found!');
+        }
+
+        return $educations;
+    }
 }
