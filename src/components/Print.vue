85b07bb15a2abd9d1bd8507b38a92f3686a2c832
KennyDepecker <kennydepecker@hotmail.com>
2017-11-08 13:49:47 +0100

    print update

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -1,11 +1,15 @@
 <template>
 
 <main>
-  <h2 class="">Rapporten afdrukken</h2>
-
-  <div class="row">
+  <v-layout row-wrap>
+      <v-flex xs12 offset-xs1 class="text-xs-left">
+        <h1 class="display-3">Rapporten afdrukken</h1>
+      </v-flex>
+  </v-layout>
+  <v-layout row-wrap>
       <p style="color: #9e9e9e">Selecteer opleidingen</p>
-
+      <v-checkbox  v-bind:label="`Alles selecteren`" v-model="allSelect" @click="changeOpleidingCheckboxStates(allSelect)" light></v-checkbox>
+      <v-checkbox v-for="(item, i) in opleiding"  v-bind:label="`Opleiding ${i}`" v-model="opleiding[i]" light></v-checkbox>
 
       <form class="col s7" action="#">
           <p>
@@ -14,26 +18,23 @@
           </p>
           <table class="striped bordered">
               <tr><th>Student</th></tr>
-
-                      <tr data-opleidingId="">
-                          <td>
-                              <p class="opleiding-checkbox " style="display:inline-block">
-                                  <input type="checkbox" id="student-checkbox" checked="checked"/>
-                                  <label
-                                      for="student-checkbox"></label>
-                              </p>
-                              <a class="waves-effect waves-light btn tooltipped right" data-delay="50"
-                                 data-tooltip="Rapport bekijken"><i class="material-icons right">import_contacts</i>Rapport</a>
-                          </td>
-                      </tr>
-
+              <tr data-opleidingId="">
+                  <td>
+                      <p class="opleiding-checkbox " style="display:inline-block">
+                          <input type="checkbox" id="student-checkbox" checked="checked"/>
+                          <label for="student-checkbox"></label>
+                      </p>
+                      <a class="waves-effect waves-light btn tooltipped right" data-delay="50"
+                         data-tooltip="Rapport bekijken"><i class="material-icons right">import_contacts</i>Rapport</a>
+                  </td>
+              </tr>
           </table>
       </form>
       <div class="right-align col s2 reports-btns">
           <a class="waves-effect waves-light btn tooltipped" style="margin-bottom: 5px" data-delay="50" data-tooltip="Rapporten downloaden"><i class="material-icons">file_download</i></a>
           <a class="waves-effect waves-light btn tooltipped" style="margin-bottom: 5px" data-delay="50" data-tooltip="Rapporten afdrukken"><i class="material-icons">print</i></a>
       </div>
-  </div>
+    </v-layout>
 </main>
 </template>
 
@@ -42,12 +43,18 @@ export default {
   name: 'Print',
   data () {
     return {
-
+      allSelect: true,
+      opleiding: [true, true, true, true, true]
+    }
+  },
+  methods: {
+    changeOpleidingCheckboxStates (val) {
+      this.opleiding = [val, val, val, val, val]
+      this.allSelect = val
     }
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
