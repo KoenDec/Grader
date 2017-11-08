0a14bc7ba5cc79e83056b177853ca3661b8f3820
KennyDepecker <kennydepecker@hotmail.com>
2017-11-08 14:35:37 +0100

    print page checkbox updates

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -7,10 +7,12 @@
       </v-flex>
   </v-layout>
   <v-layout row-wrap>
+    <v-flex xs offset-xs1>
       <p style="color: #9e9e9e">Selecteer opleidingen</p>
-      <v-checkbox  v-bind:label="`Alles selecteren`" v-model="allSelect" @click="changeOpleidingCheckboxStates(allSelect)" light></v-checkbox>
-      <v-checkbox v-for="(item, i) in opleiding"  v-bind:label="`Opleiding ${i}`" v-model="opleiding[i]" light></v-checkbox>
-
+      <v-checkbox  v-bind:label="`Alles selecteren`" v-model="allSelect" @change="changeOpleidingCheckboxStates(allSelect)" light></v-checkbox>
+      <v-checkbox v-for="(item, i) in opleiding" v-bind:label="`Opleiding ${i}`" v-model="opleiding[i]" light></v-checkbox>
+    </v-flex>
+<!--
       <form class="col s7" action="#">
           <p>
               <input type="checkbox" id="more-checkboxes" checked="checked" />
@@ -33,7 +35,7 @@
       <div class="right-align col s2 reports-btns">
           <a class="waves-effect waves-light btn tooltipped" style="margin-bottom: 5px" data-delay="50" data-tooltip="Rapporten downloaden"><i class="material-icons">file_download</i></a>
           <a class="waves-effect waves-light btn tooltipped" style="margin-bottom: 5px" data-delay="50" data-tooltip="Rapporten afdrukken"><i class="material-icons">print</i></a>
-      </div>
+      </div>-->
     </v-layout>
 </main>
 </template>
@@ -43,15 +45,26 @@ export default {
   name: 'Print',
   data () {
     return {
-      allSelect: true,
       opleiding: [true, true, true, true, true]
     }
   },
   methods: {
     changeOpleidingCheckboxStates (val) {
       this.opleiding = [val, val, val, val, val]
-      this.allSelect = val
+    },
+    checkState (val) {
+      return val
     }
+  },
+  computed: {
+    allSelect () {
+      if (this.opleiding.every(this.checkState)) {
+        return true
+      } else {
+        return false
+      }
+    }
+
   }
 }
 </script>
