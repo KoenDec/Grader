58f7861f2a09513a823a19c2d029c241052120b8
Thomas De Nil <thomas.de.nil@student.howest.be>
2017-11-08 11:32:59 +0100

    Replicating old reports page in vue

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -1,15 +1,19 @@
 <template>
   <main>
     <v-layout row-wrap>
-        <v-flex xs12>
+        <v-flex xs12 offset-xs1 class="text-xs-left">
           <h1 class="display-3">Rapporten</h1>
         </v-flex>
     </v-layout>
     <v-layout row-wrap>
-        <v-flex xs12>
-          <v-card dark color="secondary">
-            <v-card-text class="px-0">6</v-card-text>
-          </v-card>
+        <v-flex xs6 offset-xs1>
+            <v-toolbar>
+              <v-text-field  prepend-icon="search" hide-details single-line></v-text-field>
+            </v-toolbar>
+        </v-flex>
+        <v-flex>
+          <v-btn large color="primary"><v-icon>vertical_align_bottom</v-icon></v-btn>
+          <v-btn large color="primary"><v-icon></v-icon></v-btn>
         </v-flex>
     </v-layout>
   </main>
