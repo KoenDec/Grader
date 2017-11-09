3c8961245308b0e054b37ef9ff2f6ac63ecc1347
KennyDepecker <kennydepecker@hotmail.com>
2017-11-09 10:36:54 +0100

    Print page added table

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -6,30 +6,45 @@
         <h1 class="display-3">Rapporten afdrukken</h1>
       </v-flex>
   </v-layout>
-  <v-layout row-wrap>
-  <v-flex xs2>
-    <checkboxes></checkboxes>
+  <v-layout row-wrap class="mt-5">
+  <v-flex xs2 offset-xs1>
+    <checkboxes :listobject="opleidingen"></checkboxes>
+  </v-flex>
+  <v-flex xs6>
+    <v-data-table
+      v-bind:headers="headers"
+      v-bind:items="items"
+      v-bind:search="search"
+      v-model="selected"
+      item-key="student"
+      select-all
+      class="elevation-1"
+    >
+      <template slot="headerCell" scope="props">
+        <v-tooltip bottom>
+          <span slot="activator">
+            {{ props.header.text }}
+          </span>
+          <span>
+            {{ props.header.text }}
+          </span>
+        </v-tooltip>
+      </template>
+      <template slot="items" scope="props">
+        <td>
+          <v-checkbox
+            primary
+            hide-details
+            v-model="props.selected"
+          ></v-checkbox>
+        </td>
+        <td class="text-xs-left">{{ props.item.student }}</td>
+        <td class="text-xs-left">{{ props.item.opleiding }}</td>
+      </template>
+    </v-data-table>
   </v-flex>
 <!--
-      <form class="col s7" action="#">
-          <p>
-              <input type="checkbox" id="more-checkboxes" checked="checked" />
-              <label for="more-checkboxes">Alles selecteren</label>
-          </p>
-          <table class="striped bordered">
-              <tr><th>Student</th></tr>
-              <tr data-opleidingId="">
-                  <td>
-                      <p class="opleiding-checkbox " style="display:inline-block">
-                          <input type="checkbox" id="student-checkbox" checked="checked"/>
-                          <label for="student-checkbox"></label>
-                      </p>
-                      <a class="waves-effect waves-light btn tooltipped right" data-delay="50"
-                         data-tooltip="Rapport bekijken"><i class="material-icons right">import_contacts</i>Rapport</a>
-                  </td>
-              </tr>
-          </table>
-      </form>
+
       <div class="right-align col s2 reports-btns">
           <a class="waves-effect waves-light btn tooltipped" style="margin-bottom: 5px" data-delay="50" data-tooltip="Rapporten downloaden"><i class="material-icons">file_download</i></a>
           <a class="waves-effect waves-light btn tooltipped" style="margin-bottom: 5px" data-delay="50" data-tooltip="Rapporten afdrukken"><i class="material-icons">print</i></a>
@@ -43,23 +58,30 @@ export default {
   name: 'Print',
   data () {
     return {
-      allSelect: true,
-      opleiding: [true, true, true, true, true]
-    }
-  },
-  methods: {
-    changeOpleidingCheckboxStates (val) {
-      this.opleiding = [val, val, val, val, val]
-    },
-    checkState (val) {
-      return val
-    },
-    checkAll (val) {
-      if (this.opleiding.every(this.checkState)) {
-        this.allSelect = true
-      } else {
-        this.allSelect = false
-      }
+      search: '',
+      selected: [],
+      headers: [
+        { text: 'Student', align: 'left', value: 'student' },
+        { text: 'Opleiding', align: 'left', value: 'opleiding' }
+      ],
+      items: [
+        {
+          value: false,
+          student: 'MuslimParents Suicidebomber',
+          opleiding: 'Drank'
+        },
+        {
+          value: false,
+          student: 'Pleblord gayParentsFaggot',
+          opleiding: 'sletten'
+        },
+        {
+          value: false,
+          student: 'Dickbutt Quak',
+          opleiding: 'Drank'
+        }
+      ],
+      opleidingen: ['drank', 'sletten', 'kapper']
     }
   }
 }
