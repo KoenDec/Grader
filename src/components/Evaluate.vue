c8cf78ccbb7a1221fce51587c499441c4e6b602c
KennyDepecker <kennydepecker@hotmail.com>
2017-11-29 19:16:32 +0100

    push to desktop

--- src/components/Evaluate.vue
+++ src/components/Evaluate.vue
@@ -10,6 +10,37 @@
                 <h2 class="headline">Evaluatiefiche voor: {{ student.firstname }} {{student.name}}</h2>
             </v-flex>
         </v-layout>
+        <v-layout row-wrap>
+            <v-flex>
+                <v-breadcrumbs>
+                    <v-icon slot="divider">chevron_right</v-icon>
+                    <v-breadcrumbs-item
+                            v-for="breadcrumb in breadcrumbs" :key="breadcrumb.id" :disabled="breadcrumb.disabled"
+                    >
+                        {{ breadcrumb.text }}
+                    </v-breadcrumbs-item>
+                </v-breadcrumbs>
+            </v-flex>
+        </v-layout>
+        <v-layout row-wrap>
+            <v-flex xs12 sm4 offset-xs1>
+                <v-select
+                        label="Select"
+                        v-bind:items="modulesDropdown"
+                        v-model="e1"
+                        hint="Selecteer een module"
+                        persistent-hint
+                        @select-module="useModule"
+                ></v-select>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <table>
+                <tr>
+                    <th></th>
+                </tr>
+            </table>
+        </v-layout>
     </div>
 </template>
 
@@ -18,7 +49,20 @@
       name: 'Studenten',
       data () {
         return {
-          student: {name: '', firstname: ''}
+          student: {name: '', firstname: ''},
+          breadcrumbs: [
+              {id: 0, text: 'test', disabled: false},
+              {id: 1, text: '', disabled: false}
+          ],
+          modulesDropdown: [],
+          modules: [],
+          e1: []
+        }
+      },
+      methods: {
+        selectItem: function () {
+        },
+        useModule (payload) {
         }
       },
       created () {
@@ -29,6 +73,20 @@
             console.log(response.data)
             self.student.firstname = response.data.student.firstname
             self.student.name = response.data.student.lastname
+            self.breadcrumbs[0].text = response.data.opleiding.name
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+
+        this.$http.get('http://146.185.183.217/api/evaluatieVoorStudent?id=' + studentId)
+          .then(function (response) {
+            console.log(response.data)
+            self.modules = response.data.modules
+            console.log(self.modules)
+            for (var i = 0; i < self.modules.length; i++) {
+              self.modulesDropdown.push(self.modules[i].name)
+            }
           })
           .catch(function (error) {
             console.log(error)
