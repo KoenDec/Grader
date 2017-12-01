cd7b10b5c58127dd3a56f909b6e3f6fd022e78a9
KennyDepecker <kennydepecker@hotmail.com>
2017-12-01 16:51:03 +0100

    eval update

--- src/components/Evaluate.vue
+++ src/components/Evaluate.vue
@@ -41,11 +41,53 @@
                 </div>
             </v-flex>
         </v-layout>
-        <v-layout v-if="newEvalTable && moduleSelected" row wrap>
+        <v-layout v-if="newEvalTable" row wrap>
             <v-flex offset-xs1>
+                <v-layout row-wrap v-for="(cat, i) in selectedModule[0].categorieen">
+                    <v-flex xs12>
+                        <v-card color="cyan darken-1" class="white--text text-xs-center display-1" height="100%">
+                            <v-container fluid grid-list-lg fill-height>
+                                {{i+1}} {{cat.name}}
+                            </v-container>
+                        </v-card>
+                    </v-flex>
+                    <v-flex xs10>
+                        <v-layout row-wrap v-for="(doel, j) in cat.doelstellingen">
+                            <v-flex xs2>
+                                <v-card color="cyan darken-3" class="white--text text-xs-left" height="100%">
+                                    <v-container fluid grid-list-lg>
+                                        {{i+1}}.{{j+1}} {{doel.name}}
+                                    </v-container>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs10>
+                                <v-layout row-wrap v-for="(crit, k) in doel.criteria">
+                                    <v-flex xs2>
+                                        <v-card color="cyan darken-3" class="white--text text-xs-left" height="100%">
+                                            <v-container fluid grid-list-lg>
+                                                {{i+1}}.{{j+1}}.{{k+1}} {{crit.name}}
+                                            </v-container>
+                                        </v-card>
+                                    </v-flex>
+                                    <v-flex xs10>
+                                        <v-layout row-wrap v-for="(aspect, l) in crit.aspecten">
+                                            <v-flex xs4>
+                                                <v-card color="cyan darken-3" class="white--text text-xs-left">
+                                                    <v-container fluid grid-list-lg>
+                                                        {{i+1}}.{{j+1}}.{{k+1}}.{{l+1}} {{aspect.name}}
+                                                    </v-container>
+                                                </v-card>
+                                            </v-flex>
+                                        </v-layout>
+                                    </v-flex>
+                                </v-layout>
+                            </v-flex>
+                        </v-layout>
+                    </v-flex>
+                </v-layout>
                 <table>
                     <tr>
-                        <th rowspan="2">
+                        <th rowspan="2" colspan="3">
                             <v-flex>
                                 <v-text-field
                                         name="EvalFicheName"
@@ -58,9 +100,24 @@
                     <tr>
                         <th colspan="2">Evaluatie (JA | NEE)</th><th>Eind Evaluatie</th>
                     </tr>
-                    <v-flex v-for="cat in selectedModule.categorieen" :key="cat.id">
-                        <tr><th rowspan="4">{{cat.name}}</th></tr>
-                    </v-flex>
+                    <template v-for="cat in selectedModule[0].categorieen">
+                        <tr><th>{{cat.name}}</th><td colspan="5"></td></tr>
+                        <template v-for="doel in cat.doelstellingen">
+                            <tr>
+                                <td :rowspan="1">{{doel.name}}</td>
+                            </tr>
+                            <template v-for="crit in doel.criteria">
+                                <tr>
+                                    <td :rowspan="crit.aspecten.length">{{crit.name}}</td>
+                                </tr>
+                                <template v-for="aspect in crit.aspecten">
+                                    <tr>
+                                        <td>{{aspect.name}}</td><td class="empty-cel"></td><td class="empty-cel"></td><td class="empty-cel"></td>
+                                    </tr>
+                                </template>
+                            </template>
+                        </template>
+                    </template>
                 </table>
             </v-flex>
         </v-layout>
@@ -83,7 +140,8 @@
           selectedModule: {},
           evalFiches: [],
           moduleSelected: false,
-          newEvalTable: false
+          newEvalTable: false,
+          doelRowSpan: 0
         }
       },
       methods: {
@@ -96,6 +154,7 @@
           this.selectedModule = result
           console.log(this.selectedModule)
           this.moduleSelected = true
+          this.newEvalTable = false
         },
         newEval: function () {
           this.newEvalTable = true
@@ -133,7 +192,11 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-th, tr{
+th, tr, td{
     border: 1px black solid
+}
+.empty-cel{
+    width: 50px;
+    height:50px;
 }
 </style>
