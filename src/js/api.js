d98f77f879a6ce0d5f031e22534b347cb7516cda
KennyDepecker <kennydepecker@hotmail.com>
2018-01-25 19:57:15 +0100

    update werkplaats commentaar

--- src/js/api.js
+++ src/js/api.js
@@ -131,6 +131,7 @@ var Api = (function () {
     axios.post('http://146.185.183.217/api/saveReport', {
       commentaarAlgemeen: report.commentaarAlgemeen,
       commentaarKlassenraad: report.commentaarKlassenraad,
+      commentaarWerkplaats: report.commentaarWerkplaats,
       enddate: report.enddate,
       modules: report.modules,
       name: report.name,
@@ -150,6 +151,7 @@ var Api = (function () {
     axios.post('http://146.185.183.217/api/updateReport', {
       commentaarAlgemeen: report.commentaarAlgemeen,
       commentaarKlassenraad: report.commentaarKlassenraad,
+      commentaarWerkplaats: report.commentaarWerkplaats,
       enddate: report.enddate,
       modules: report.modules,
       name: report.name,
